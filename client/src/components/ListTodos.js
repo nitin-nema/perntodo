37604a2fb97a3afa1335121571a4644202c30baa
nitin-nema <nitinnema89@gmail.com>
2022-11-29 11:34:00 +0530

    Edit/delete functionality

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,12 +1,25 @@
-import React,{useEffect} from 'react';
+import React,{useState, useEffect} from 'react';
 
-
+import EditTodo from "./EditTodos";
 function ListTodos() {
+    const [todos, setTodos] = useState([]);
+
+    const deleteTodo = async id => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            })
+
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
     const getTodos = async() => {
         try {
-            const response = await fetch("http://localhost:5000/todos")
-            const jsonData = response.json();
-            console.log(jsonData);
+            const response = await fetch("http://localhost:5000/todos");
+            const jsonData = await response.json();
+            setTodos(jsonData);
         } catch (error) {
             console.log(error.message);
         }
@@ -14,10 +27,11 @@ function ListTodos() {
 
     useEffect(() => {
         getTodos();
-    },[]);
+    }, []);
+
     return (
         <div>
-           <table class="table mt-5 text-center">
+           <table className="table mt-5 text-center">
                 <thead>
                 <tr>
                     <th>Description</th>
@@ -26,13 +40,18 @@ function ListTodos() {
                 </tr>
                 </thead>
                 <tbody>
+                    {/* {JSON.stringify(todos)} */}
+                    {todos.map(todo => (
+                        <tr key= {todo.todo_id}>
+                            <td>{todo.description}</td>
+                            <td><EditTodo todo={ todo}/></td>
+                            <td><button className = "btn btn-danger" onClick = {() => deleteTodo(todo.todo_id)}>Delete</button></td>
+                        </tr>
+                    ))
                     
-                        <tr>
-                        <td>John</td>
-                        <td>Doe</td>
-                        <td>john@example.com</td>
-                    </tr>
+                    }
                     
+
                 </tbody>
             </table>
 
